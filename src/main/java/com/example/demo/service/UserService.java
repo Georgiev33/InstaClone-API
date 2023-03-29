faf5605c6a39ba06fe48ad687b057ee1b3e32667
Bace_kz <vasildinchev@gmail.com>
2023-03-29 16:29:21 +0300

    Refactored createUser method in User Service

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.demo.model.dto.UserRegistrationDTO;
 import com.example.demo.model.entity.User;
 import com.example.demo.model.exception.BadRequestException;
 import com.example.demo.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.internal.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,9 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -35,53 +38,60 @@ public class UserService {
         if (!encoder.matches(userLoginDTO.getPassword(), user.getPassword())) {
             throw new BadRequestException("Bad credentials");
         }
-        if(!user.isVerified()){
+        if (!user.isVerified()) {
             throw new BadRequestException("Your account isn`t verified");
         }
         return user.getId();
     }
 
     public void createUser(UserRegistrationDTO userRegistrationDTO) {
-        if (!userRegistrationDTO.getPassword().equals(userRegistrationDTO.getConfirmPassword())) {
-            throw new BadRequestException("Passwords must match");
-        }
-        if (isUserAvailable(userRegistrationDTO.getUsername())) {
-            throw new BadRequestException("Username already exist");
-        }
-        if (isEmailAvailable(userRegistrationDTO.getEmail())) {
-                throw new BadRequestException("Email already exist");
-        }
-
+        validateUser(userRegistrationDTO);
         User user = modelMapper.map(userRegistrationDTO, User.class);
         user.setPassword(encoder.encode(userRegistrationDTO.getPassword()));
-        String verificationCode = RandomString.make(64);
-        user.setVerificationCode(verificationCode);
-        simpleMailMessage.setTo(userRegistrationDTO.getEmail());
-        simpleMailMessage.setText("http://localhost:8080/user/" + verificationCode);
-        simpleMailMessage.setSubject("do.not.reply");
-        mailSender.send(simpleMailMessage);
+        user.setVerificationCode(sendVerificationEmail(userRegistrationDTO));
         userRepository.save(user);
     }
 
-    public ResponseEntity<String> verifyUser(String verificationCode){
+    public ResponseEntity<String> verifyUser(String verificationCode) {
         User user = userRepository.findUserByVerificationCodeAndVerificationCodeIsFalse(verificationCode)
-                .orElseThrow(() ->new BadRequestException("Invalid verification code"));
+                .orElseThrow(() -> new BadRequestException("Invalid verification code"));
         user.setVerified(true);
         userRepository.save(user);
         return ResponseEntity.ok("Registration successfully verified");
     }
 
-    public boolean isEmailAvailable(String email) {
+    public boolean doesEmailExist(String email) {
         Optional<User> u = userRepository.findUserByEmail(email);
         return u.isPresent();
     }
 
-    public boolean isUserAvailable(String username) {
+    public boolean doesUsernameExist(String username) {
         Optional<User> u = userRepository.findUserByUsername(username);
         return u.isPresent();
     }
 
     public Optional<User> findUserByUsername(String username) {
         return userRepository.findUserByUsername(username);
+    }
+
+    private void validateUser(UserRegistrationDTO userRegistrationDTO) {
+        if (!userRegistrationDTO.getPassword().equals(userRegistrationDTO.getConfirmPassword())) {
+            throw new BadRequestException("Passwords must match");
+        }
+        if (doesUsernameExist(userRegistrationDTO.getUsername())) {
+            throw new BadRequestException("Username already exist");
+        }
+        if (doesEmailExist(userRegistrationDTO.getEmail())) {
+            throw new BadRequestException("Email already exist");
+        }
+    }
+
+    private String sendVerificationEmail(UserRegistrationDTO userRegistrationDTO) {
+        String verificationCode = RandomString.make(64);
+        simpleMailMessage.setTo(userRegistrationDTO.getEmail());
+        simpleMailMessage.setText("http://localhost:8080/user/" + verificationCode);
+        simpleMailMessage.setSubject("do.not.reply");
+        new Thread(() -> mailSender.send(simpleMailMessage)).start();
+        return verificationCode;
     }
 }
